f7e3a8ce5d302bf2154d4979df7411ac468e6b10
Cida Rosa <lucita.rosa@gmail.com>
2023-02-14 19:18:51 -0300

    Aula 09 - Comentários

---
+++ aulas/Aula009_Comentarios/src/curso/Comentarios.java
@@ -0,0 +1,37 @@
+package curso;
+
+/**
+ * Programa para exemplificar o uso de comentário
+ * @author Cida Castello
+ * @version 1.0
+ * @since 01/2023
+ *
+ */
+
+public class Comentarios {
+	
+	public static void main(String[] args) {
+		
+		// comentário em linha
+		
+		/*
+		 * comentário em bloco
+		 */
+		
+		//System.out.println("Primeira linha");
+		
+		//escreve no terminal a frase Primeira linha
+		System.out.println("Primeira linha"); //saída de dados
+		
+		/*
+		System.out.println("Segunda linha");		
+		System.out.println("Terceira linha");
+		*/
+		
+		System.out.println("Depois do bloco comentado");
+		
+		
+		
+	}
+
+}
