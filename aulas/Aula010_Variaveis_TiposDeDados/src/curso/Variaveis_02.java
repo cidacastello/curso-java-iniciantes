aed51bb6043f663bad8705429a38bfea4f76d241
Cida Rosa <lucita.rosa@gmail.com>
2023-02-25 19:15:53 -0300

    Aula 10 - Varíáveis e tipos de dados - parte 1

---
+++ aulas/Aula010_Variaveis_TiposDeDados/src/curso/Variaveis_02.java
@@ -0,0 +1,35 @@
+package curso;
+
+/**
+ * 
+ * @author Cida
+ * Declaração de variáveis
+ *
+ */
+public class Variaveis_02 {
+	
+	public static void main(String[] args) {
+		
+		//declaração de variáveis
+		//<tipo> <nome da variável>;
+		//<tipo> <nome da variável> = <valor;
+		//tipo inteiro
+		int a; //declaração da variável
+		a = 5; //atribuição do valor à variável
+		//declarando e inicializando a variável
+		int b = 3;
+		
+		System.out.println(a + b);
+		//declarar variável que vai ter a soma de a + b
+		int soma = a + b;
+		System.out.println(soma);
+		
+		
+	}
+
+}
+
+
+
+
+
---
+++ aulas/Aula010_Variaveis_TiposDeDados/src/curso/Variavies_01.java
@@ -0,0 +1,19 @@
+package curso;
+
+/**
+ * 
+ * @author Cida
+ * Programa para efetuar a soma de 5 + 3
+ *
+ */
+
+public class Variavies_01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(5 + 3);
+		
+		
+	}
+
+}
