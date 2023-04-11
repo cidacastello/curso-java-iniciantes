3de99e0aef34faa460942dcf7c5192ed67bb82b6
Cida Rosa <lucita.rosa@gmail.com>
2023-04-11 11:53:34 -0300

    Aula 016 - Update Lista Exercicios

---
+++ aulas/Aula016_OperadoresAritmeticos/src/curso/OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+package curso;
+
+public class OperadoresAritmeticos {
+	
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		double z = 2.5;
+		/*
+		  Operadores
+		  +  -  *  /   %
+		 */
+		
+		 double resultado = a % z;
+		 System.out.println("Resultado: " + resultado);
+	}
+
+}
+
+
+
+
+
+
---
+++ aulas/Aula016_OperadoresAritmeticos/src/curso/TestaDivisao.java
@@ -0,0 +1,34 @@
+package curso;
+
+/*
+Operadores
+  /   %
+*/
+public class TestaDivisao {
+	
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 3;
+		
+		int resultado1 = x / y;
+		System.out.println("Resultado1: " + resultado1);
+		int resto = x % y;
+		System.out.println("Resto: " + resto);
+		float resultado2 = x / y;
+		System.out.println("Resultado2: " + resultado2);
+		double resultado3 = (double)x / (double)y;
+		System.out.println("Resultado3: " + resultado3);
+		
+		//System.out.println(7 / 4);
+		
+	}
+	
+
+}
+
+
+
+
+
+
