f3fe977845003b70846b742dcbac46a53fdae54a
cidarosa <lucita.rosa@gmail.com>
2023-07-31 20:49:35 -0300

    Aula 28

---
+++ aulas/Aula028_CondicionalAninhada/src/curso/CondicionalAninhada.java
@@ -0,0 +1,21 @@
+package curso;
+
+public class CondicionalAninhada {
+	
+	public static void main(String[] args) {
+		
+		int x = 3, y = 4;
+
+		if (x > 5) { 
+			if (y > 5) { 
+				System.out.println("x e y são > 5");
+			}			
+		}else {
+			System.out.println("x é < 5");
+		}
+		
+		
+			
+	}
+
+}
---
+++ aulas/Aula028_CondicionalAninhada/src/curso/CondicionalAninhada2.java
@@ -0,0 +1,18 @@
+package curso;
+
+public class CondicionalAninhada2 {
+
+	public static void main(String[] args) {
+		int primeiro = 8;
+		int segundo = 3;
+
+		if (primeiro == segundo) {
+			System.out.println("Os números são iguais");
+		}else if (primeiro > segundo) {
+			System.out.println("Primeiro é maior");
+		}else {
+			System.out.println("Segundo é maior");
+		}
+	}
+
+}
