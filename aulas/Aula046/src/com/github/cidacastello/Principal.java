00c7045dc05341b16cf03da5889ef8993d6d1c9d
cidarosa <lucita.rosa@gmail.com>
2024-06-18 18:18:11 -0300

    Aula 46

---
+++ aulas/Aula046/src/com/github/cidacastello/Principal.java
@@ -0,0 +1,26 @@
+//pacote
+package com.github.cidacastello;
+
+// imports
+import com.github.cidacastello.modelo.Carro;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, minha primeira classe!");
+		// criando objeto do tipo Carro
+		// tipo - nome = new construtor da classe
+		Carro carro = new Carro();
+		
+
+	}
+
+}
+
+
+
+
+
+
+
---
+++ aulas/Aula046/src/com/github/cidacastello/modelo/Carro.java
@@ -0,0 +1,20 @@
+package com.github.cidacastello.modelo;
+
+//declaração da classe
+
+public class Carro {
+	
+	// variáves de instância (atributos, campos)
+	// tipo - nome da variável de instância
+	
+	String marca;
+	String modelo;
+	int anoFrabricacao;
+	String cor;
+	double preco;
+	
+	
+	// métodos
+	
+
+}
